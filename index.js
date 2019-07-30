96bd0a13f9e30acf4fdfe1eb6f473e7b1438a244
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2019-07-30 21:17:48 +0200

    cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-// testing
-
-// run before trying to access the app
-// export GOOGLE_APPLICATION_CREDENTIALS="/Users/tomek/Developer/dev-helpers/config/translateApiKey.json"
-const quickstart = require('./google-functions/translateLocal');
-
-quickstart();
